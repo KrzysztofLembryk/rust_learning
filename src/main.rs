2f97d39242bc72fb60dbf30aa16c7cdbf354597c
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2025-10-19 12:46:16 +0200

    started chapter 4 - ownership, ended chapter 3

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,97 @@ use std::io;
 use rand::Rng;
 
 fn main() {
- chapter_3_common_programming_concepts();
+    chapter_4_OWNERSHIP();
+}
+
+fn chapter_4_OWNERSHIP()
+{
+    let is_ownership = true;
+    let is_string_type = true;
+
+    if is_ownership {
+        println!("############# OWNERSHIP #############");
+
+        // OWNERSHIP RULES:
+        // -- Each value in Rust has an owner.
+        // -- THERE CAN ONLY BE ONE OWNER AT A TIME.
+        // -- When the owner goes out of scope, the value will be dropped.
+
+        // The variable s  is valid from the point at which it’s declared UNTIL THE END OF THE CURRENT SCOPE.
+
+        { // we create a local scope
+            let s = "some variable";
+            println!("s value inside scope: {s}");
+        } 
+        // the scope is over, so s is NO LONGER VALID
+        // this will cause compiler error
+        // println!("value of s: {s}");
+        // So:
+        // - When s comes into scope, it is valid.
+        // - It remains valid until it goes out of scope.
+
+    }
+
+    if is_string_type {
+        println!("############# STRING TYPE #############");
+        // We want to look at data that is stored on the heap and explore how 
+        // Rust knows when to clean up that data, String type is a great example
+
+        // string literal type, a string value is hardcoded into our program
+        // we cannot modify it
+        let str_literal = "string literal value";
+        println!("str literal: {}", str_literal);
+
+        // String type: manages data allocated on the heap and as such is able 
+        // to store an unknown amount of text 
+
+        // creating a mutable string
+        let mut mutable_str = String::from("allocated data string");
+
+        mutable_str.push_str(", newly pushed string");
+
+        println!("mutable str: {mutable_str}");
+
+        // WHY we cannot modify string literal? We know the contents at compile time, so the text is hardcoded directly into the final executable
+        // With String type we allocate memoty on heap, unknown at compile time
+        // This means:
+        // -- The memory must be requested from the memory allocator at runtime.
+        //     ---> this is done by us using String::from
+        // -- We need a way of returning this memory to the allocator when we’re done with our String.
+        //     ---> this is where ownership comes
+
+        // Changing ownership etc
+        println!("########### Variables and Data Interacting with Move ###########");
+
+        // since integers are simple data types with known FIXED size, thus
+        // they will be copied and pushed on stack  
+        let x = 5;
+        let y = x;
+        println!("x = {x}, y = {y}");
+
+        // HOWEVER this is not the case with below example
+        let s1 = String::from("some string");
+        let s2 = s1;
+
+        // s2 will not have a copy of s1, but s2 will BE THE NEW AND ONLY OWNER
+        // of s1 string data, thus after s2 = s1 line, s1 is NO LONGER VALID
+        // and does not point to our String data 
+
+    }
+
 }
 
 fn chapter_3_common_programming_concepts()
 {
+    let is_variables = true;
+    let is_data_types = true;
+    let is_functions = true;
+    let is_conditions = true;
+    let is_loops = true;
     // --------- VARIABLES ---------
-    {
+    
+    if is_variables {
+        println!("############# VARIABLES #############");
         // by default variables are IMMUTABLE
         let x = 5;
         // x = 69 won't compile
@@ -45,7 +129,8 @@ fn chapter_3_common_programming_concepts()
     }
 
     // --------- DATA TYPES ---------
-    {
+    if is_data_types {
+        println!("############# DATA TYPES #############");
         // INTEGER OVERFLOW
         // Let’s say you have a variable of type u8 that can hold values between 0 and 255. If you try to change the variable to a value outside that range, such as 256, integer overflow will occur, which can result in one of two behaviors. When you’re compiling IN DEBUG MODE, Rust includes CHECKS FOR INTEGER OVERFLOW that cause your program to panic at runtime if this behavior occurs. Rust uses the term panicking when a program exits with an error
 
@@ -112,6 +197,121 @@ fn chapter_3_common_programming_concepts()
     }
 
     // --------- FUNCTIONS ----------
+    if is_functions {
+        println!("############# FUNCTIONS #############");
+        // Rust doesn’t care where you define your functions, can be before or
+        // after the main function, it still will be visible
+        // STATEMENTS AND EXPRESSIONS
+        // -> Statements are instructions that perform some action and do not return a value.
+        // -> Expressions evaluate to a resultant value.
+
+        let y = 69; // this is statement, does not return value
+
+        // let x = (let y = 6); this will not compile since statements do not
+        // return values
+        // In C this will compile int i = j = 6; but not in Rust
+        
+        // Expressions can be part of statement
+        let y = {
+            let x = 3;
+            x + 1
+        };
+
+        // function declaration:
+        fn plus_one(x: i32) -> i32 {
+            x + 1 // this is an expression, thus it evaluates, thus we dont need ;
+        }
+
+        // But if we did x+1;, we would get an error, since x+1; is no longer 
+        // an expression, but a statement
+    }
+
+    // --------- CONDITIONS ----------
+    if is_conditions {
+        println!("############# CONDITIONS #############");
+        // conditions in IF must be a BOOL
+        let val = 3;
+        if  val > 0 
+        {
+            println!("val: {val} > 0");
+        }
+
+        // This will not compile since VAL IS NOT BOOl
+        // if val {
+        //     println!("Won't compile since statement in if is not bool but 3")
+        // }
+
+        // BECAUSE IF IS AN EXPRESSION, we can use it on the right side of a let statement to assign the outcome to a variable
+        let condition = true;
+        let number = if condition { 5 } else { 6 };
+
+        println!("Using if in rhs of let succeded,value of number is:{number}");
+    }
+
+    // ---------- LOOPS -----------
+    if is_loops {
+        println!("############# LOOPS #############");
+        // we have three loop key-words:
+        // - loop
+        // - for
+        // - while
+        
+        // ###### LOOP ######
+        // -> will loop forever
+        // One of the uses of a loop is to retry an operation you know might fail, such as checking whether a thread has completed its job. You might also need to PASS THE RESULT OF THAT OPERATION OUT OF THE LOOP to the rest of your code. To do this, you can ADD THE VALUE YOU WANT RETURNED AFTER THE BREAK expression you use to stop the loop; that value will be returned out of the loop
+
+        let mut counter = 0;
+        let result = loop {
+            counter += 1;
+            if counter > 10 
+            {
+                break counter * counter
+            }
+        };
+        println!("result of breaking from loop {result}");
+
+        // !!!!!!!!!!!  LOOP LABELS  !!!!!!!!!!!!!!
+        let mut count = 0;
+        
+        println!("--LOOP LABELS--");
+        'main_loop: loop {
+            println!("main lloopp count: {count}");
+
+            let mut nbr_of_loops = 3;
+
+            loop {
+                println!("nbr_of_loops: {nbr_of_loops}");
+
+                if nbr_of_loops < 3 
+                {
+                    break;
+                }
+                if count == 1
+                {
+                    println!("breaking main loop from inner loop");
+                    break 'main_loop;
+                }
+                nbr_of_loops -= 1;
+            }
+            count += 1;
+        }
+        println!("End of main loop");
+
+        // WHILE looping is the same as in C, but IT IS SLOWER than FOR looping
+        // when we loop through an array. Why?
+        // Because the COMPILER ADDS RUNTIME CODE to perform the CONDITIONAL 
+        // CHECK of whether the INDEX IS WITHIN THE BOUNDS of the array on every iteration through the loop
+
+        // for loop with ranges
+        println!("-- FOR LOOP WITH RANGES --");
+
+        for nbr in (1..=5).rev()
+        {
+            print!("{nbr},");
+        }
+        println!();
+    }
+
 }
 
 fn chapter_2_guessing_game()
